890da85d8240e38d8578b6397f1d8a46e5830494
Jihad Mohammed <68543921+the-introvertguy@users.noreply.github.com>
2024-11-01 08:24:23 +0600

    Update 1715.c
    updated

--- FIles/1715.c
+++ FIles/1715.c
@@ -1,31 +1,23 @@
-#include<stdio.h>
-int main()
-{
-    int ar[101][101],i,j,n,m,cnt=0,final=0;
-    scanf("%d %d",&n,&m);
-    for(i=0; i<n; i++)
-    {
-        for(j=0; j<m; j++)
-        {
-            scanf("%d",&ar[i][j]);
-        }
-    }
-    for(i=0; i<n; i++)
-    {
-        for(j=0; j<m; j++)
-        {
-            if(ar[i][j]>0)
-            {
-                cnt++;
-            }
-        }
-        if(cnt==m)
-        {
-            final++;
-
-        }
-        cnt=0;
-    }
-    printf("%d\n",final);
-    return 0;
+#include <stdio.h>
+int main() {
+  int ar[101][101], i, j, n, m, cnt = 0, final = 0;
+  scanf("%d %d", &n, &m);
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < m; j++) {
+      scanf("%d", &ar[i][j]);
+    }
+  }
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < m; j++) {
+      if (ar[i][j] > 0) {
+        cnt++;
+      }
+    }
+    if (cnt == m) {
+      final++;
+    }
+    cnt = 0;
+  }
+  printf("%d\n", final);
+  return 0;
 }
